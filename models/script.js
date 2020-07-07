e55f17c576ec040d2afb08474c346e3bbb7329de
pbl6asoad <44265632+pbl6asoad@users.noreply.github.com>
2020-07-08 01:00:18 +0300

    Add files via upload

--- models/script.js
+++ models/script.js
@@ -1,4 +1,4 @@
 let vkAuth = document.querySelector('#vkAuth')
 vkAuth.addEventListener('click', () => {
-    window.location.href = "https://oauth.vk.com/authorize?client_id=7533124&redirect_uri=https://pbl6asoad.github.io&scope=0&response_type=token&v=5.120";
+    window.location.href = "https://oauth.vk.com/authorize?client_id=7533124&redirect_uri=https://pbl6asoad.github.io/models/index&scope=0&response_type=token&v=5.120";
 })
