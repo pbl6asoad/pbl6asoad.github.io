c6f85813a366f47e7b3a9915517ecd6e385e4cba
pbl6asoad <44265632+pbl6asoad@users.noreply.github.com>
2020-07-08 01:35:38 +0300

    Add files via upload

--- models/callback.js
+++ models/callback.js
@@ -1,10 +1,10 @@
 let parametres = window.location.href.substr(window.location.href.indexOf('#') + 1, 200)
 parametres = parametres.split('&')
 parametres.forEach((element) => {
-   element = element.split('=')
-   console.log(element);
+   return element = element.split('=')
+   
 })
-// console.log(parametres);
+console.log(parametres);
 
 // console.log(getParameterByName('access_token'));
 // localStorage.setItem('access_token', )
