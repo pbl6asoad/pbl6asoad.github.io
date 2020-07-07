95a6da8442bd44d34f5e4eae39b1815c893ae1e6
pbl6asoad <44265632+pbl6asoad@users.noreply.github.com>
2020-07-08 01:10:23 +0300

    Add files via upload

--- models/callback.js
+++ models/callback.js
@@ -1,1 +1,14 @@
-console.log(window.location.href);
+var getParams = function (url) {
+	var params = {};
+	var parser = document.createElement('a');
+	parser.href = url;
+	var query = parser.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
+};
+console.log(getParams(window.location.href));
+// localStorage.setItem('access_token', )
