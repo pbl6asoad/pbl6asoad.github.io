f9314f5d6e15aca13d16a99c74c7332f745c27b8
pbl6asoad <44265632+pbl6asoad@users.noreply.github.com>
2020-07-08 02:00:33 +0300

    Add files via upload

--- models/script.js
+++ models/script.js
@@ -14,16 +14,14 @@ if(localStorage.getItem('access_token')) {
           &v=5.120`, 
           {
             method: 'GET',
-            mode: 'no-cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
+            mode: 'no-cors', 
+            cache: 'no-cache',
+            credentials: 'same-origin', 
             headers: {
               'Content-Type': 'application/json'
-              // 'Content-Type': 'application/x-www-form-urlencoded',
             },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *client
-            body: JSON.stringify(data) // body data type must match "Content-Type" header
+            redirect: 'follow', 
+            referrerPolicy: 'no-referrer'
           })
           .then(res => res.json())
           .then(res => {
