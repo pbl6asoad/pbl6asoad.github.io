7dc5da7c12a9169027d2fa94f52df9ccd0885e50
pbl6asoad <44265632+pbl6asoad@users.noreply.github.com>
2020-07-08 15:25:12 +0300

    Update script.js

--- models/script.js
+++ models/script.js
@@ -4,9 +4,13 @@ vkAuth.addEventListener("click", () => {
     "https://oauth.vk.com/authorize?client_id=7533124&redirect_uri=https://pbl6asoad.github.io/models/callback&scope=0&response_type=token&v=5.120";
 });
 
-if (localStorage.getItem("access_token")) {
+if (localStorage.getItem("access_token", {
+  headers: {
+    "Access-Control-Allow-Origin":'https://api.vk.com'
+  }
+})) {
   fetch(
-    `https://cors-anywhere.herokuapp.com/https://api.vk.com/method/friends.get?user_ids=${localStorage.getItem(
+    `https://api.vk.com/method/friends.get?user_ids=${localStorage.getItem(
       "user_id"
     )}&fields=city,domain&access_token=${localStorage.getItem(
       "access_token"
