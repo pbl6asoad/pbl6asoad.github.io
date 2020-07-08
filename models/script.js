c2be28db9217ce5fa4a80832059f672cdeb60338
pbl6asoad <44265632+pbl6asoad@users.noreply.github.com>
2020-07-08 16:16:11 +0300

    Add files via upload

--- models/script.js
+++ models/script.js
@@ -1,16 +1,11 @@
 let vkAuth = document.querySelector("#vkAuth");
+let getFriends = document.querySelector("#getFriends");
 vkAuth.addEventListener("click", () => {
   window.location.href =
     "https://oauth.vk.com/authorize?client_id=7533124&redirect_uri=https://pbl6asoad.github.io/models/callback&scope=offline&response_type=token&v=5.120";
 });
 
-if (
-  localStorage.getItem("access_token", {
-    headers: {
-      "Access-Control-Allow-Origin": "https://api.vk.com",
-    },
-  })
-) {
+getFriends.addEventListener("click", () => {
   fetch(
     `https://cors-anywhere.herokuapp.com/https://api.vk.com/method/friends.get?user_ids=${localStorage.getItem(
       "user_id"
@@ -26,12 +21,24 @@ if (
       let data = [];
       data = res.response.items.map((element) => {
         return `
- <a href="vk.com/id${element.id}">
-   <p>${element.first_name} ${element.last_name}</p>
- </a>
- `;
+                <a href="vk.com/id${element.id}">
+                  <p>${element.first_name} ${element.last_name}</p>
+                </a>
+               `;
       });
       document.querySelector("p").innerHTML = data.join("");
       console.log(data);
     });
+});
+
+if (localStorage.getItem("access_token")) {
+  fetch(`https://api.vk.com/method/users.get?
+        access_token=${localStorage.getItem(
+          "access_token"
+        )}
+        &user_ids=${localStorage.getItem(
+          "user_id"
+        )}&fields=photo_50,city,verified&name_case=Nom&v=5.120`)
+        .then(res => res.json())
+        .then(res => console.log(res))
 }
