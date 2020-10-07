fbe06e4d6d5e00daa37bf516866b0971b344b127
pbl6asoad <44265632+pbl6asoad@users.noreply.github.com>
2020-10-07 21:34:30 +0300

    Add files via upload

--- mrtest/js/script.js
+++ mrtest/js/script.js
@@ -1,33 +1,39 @@
 let array 
-fetch('https://cors-anywhere.herokuapp.com/https://www.mrsoft.by/data.json')
-.then(res => res.json())
-.then(res => {
-    array = res.data
-})
-let popup        = document.querySelector('#pop-up')
-let findByLength = document.querySelector('#findByLength')
-let findBySubstr = document.querySelector('#findBySubstr')
-let input        = document.querySelector('#input')
-let checkbox     = document.querySelector('#checkbox')
-let ol           = document.querySelector('ol')
-let hided_pop_up = document.querySelector('#pop-up-hided') 
+const popup        = document.querySelector('#pop-up')
+const findByLength = document.querySelector('#findByLength')
+const findBySubstr = document.querySelector('#findBySubstr')
+const input        = document.querySelector('#input')
+const checkbox     = document.querySelector('#checkbox')
+const ol           = document.querySelector('ol')
+const hidedPopUp   = document.querySelector('#pop-up-hided') 
 
 
 
-let togglePopUp = (isShowed) => {
-   if(isShowed){
-       popup.style.display = "block"
-       ol.classList = ['blurred']
-       hided_pop_up.style.display = "none"
-   } else {
-       popup.style.display = "none"
-       ol.classList = ['']
-       hided_pop_up.style.display = "block"
-   }
+const togglePopUp = (isShowed) => {
+    if(isShowed){
+        popup.style.display = "block"
+        ol.classList = ['blurred']
+        hidedPopUp.style.display = "none"
+    } else {
+        popup.style.display = "none"
+        ol.classList = ['']
+        hidedPopUp.style.display = "block"
+    }
 }
 
-let digit = (number) => {
-    let result = []
+hidedPopUp.addEventListener('click', () => {
+    togglePopUp(true)
+})
+findByLength.addEventListener('click', () => {
+    ol.innerHTML = digit(input.value)
+})
+
+findBySubstr.addEventListener('click', () => {
+    ol.innerHTML = substr(input.value, checkbox.checked)
+})
+
+const digit = (number) => {
+    const result = []
     array.forEach(word => {
         if(word.length > number){
             result.push(`<li>${word}</li>`)
@@ -37,8 +43,8 @@ let digit = (number) => {
     return result.join('')
 }
 
-let substr = (substr, checked) => {
-    let result = []
+const substr = (substr, checked) => {
+    const result = []
     array.forEach(word => {
         if(checked === true ){
             if(word.includes(substr)) {
@@ -54,20 +60,10 @@ let substr = (substr, checked) => {
     return result.join('')
 }
 
-hided_pop_up.addEventListener('mouseenter', () => {
-    hided_pop_up.className = 'position-absolute hided-pop-up__animation-hover'
-    hided_pop_up.addEventListener('click', () => {
-        togglePopUp(true)
-    })
-})
-hided_pop_up.addEventListener('mouseleave', () => {
-    hided_pop_up.className = 'position-absolute hided-pop-up__animation-default'
-})
-findByLength.addEventListener('click', () => {
-    ol.innerHTML = digit(input.value)
-})
-
-findBySubstr.addEventListener('click', () => {
-    ol.innerHTML = substr(input.value, checkbox.checked)
-})
-
+const httpGetRequest = ((url = "https://cors-anywhere.herokuapp.com/https://www.mrsoft.by/data.json") => {
+    fetch(url)
+        .then(res => res.json())
+        .then(res => {
+            array = res.data
+        })
+})()
